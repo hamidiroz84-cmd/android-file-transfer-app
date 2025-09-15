f75f40867f046f517715dcea3ee2fcc6ac2307ce
hamidiroz84-cmd <hamidiroz84@gmail.com>
2025-09-15 07:50:39 +0330

    Add files via upload

---
+++ deepseek_bash_20250915_e9a858.sh
@@ -0,0 +1,7 @@
+# در ترمینال Android Studio اجرا کنید
+git init
+git add .
+git commit -m "First commit: Complete file transfer app"
+git branch -M main
+git remote add origin https://github.com/YOUR-USERNAME/android-file-transfer-app.git
+git push -u origin main
